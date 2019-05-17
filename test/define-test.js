5be07ab3b6d746a8b9a84ad4c9c020e07c4d42f8
Kevin Phillips <kevin@bitovi.com>
2019-05-17 12:21:26 -0500

    checking for customElements support before running tests that need it

--- test/define-test.js
+++ test/define-test.js
@@ -3,6 +3,8 @@ const { define: dFine } = require("../can-define-class");
 
 QUnit.module("can-define-class - define()");
 
+const supportsCustomElements = "customElements" in window;
+
 QUnit.test("Can define stuff", function(assert) {
   class Faves extends dFine() {
     static get define() {
@@ -44,58 +46,60 @@ QUnit.test("Stuff is defined in constructor for non-element classes", function(a
   let faves = new Faves();
 });
 
-QUnit.test("Stuff is defined in connectedCallback for custom elements", function(assert) {
-  class Faves extends dFine(HTMLElement) {
-    static get define() {
-      return {
-		  color: {
-			  default: "blue"
-		  }
-      };
-    }
-
-	constructor() {
-		super();
-		assert.equal(this.color, undefined, "color does not exist after constructor");
-	}
-
-	connectedCallback() {
-		super.connectedCallback();
-		assert.equal(this.color, "blue", "color does not exist after constructor");
-	}
-  }
-  customElements.define("can-defined-el", Faves);
-
-  let faves = new Faves();
-  faves.connectedCallback();
-});
-
-QUnit.test("Stuff is not redefined in connectedCallback for classes that extend Elements but call setup themselves", function(assert) {
-  class Faves extends dFine(HTMLElement) {
-	  static get define() {
+if (supportsCustomElements) {
+	QUnit.test("Stuff is defined in connectedCallback for custom elements", function(assert) {
+	  class Faves extends dFine(HTMLElement) {
+		static get define() {
 		  return {
 			  color: {
 				  default: "blue"
 			  }
 		  };
+		}
+
+		constructor() {
+			super();
+			assert.equal(this.color, undefined, "color does not exist after constructor");
+		}
+
+		connectedCallback() {
+			super.connectedCallback();
+			assert.equal(this.color, "blue", "color does not exist after constructor");
+		}
 	  }
+	  customElements.define("can-defined-el", Faves);
 
-	constructor() {
-		super();
-		assert.equal(this.color, undefined, "color does not exist after constructor");
-		this.setup();
-		assert.equal(this.color, "blue", "color exists after setup is called manually");
-		this.color = "red";
-		assert.equal(this.color, "red", "color is changed to red");
-	}
+	  let faves = new Faves();
+	  faves.connectedCallback();
+	});
 
-	connectedCallback() {
-		super.connectedCallback();
-		assert.equal(this.color, "red", "color is not redefined");
-	}
-  }
-  customElements.define("can-manually-defined-el", Faves);
+	QUnit.test("Stuff is not redefined in connectedCallback for classes that extend Elements but call setup themselves", function(assert) {
+	  class Faves extends dFine(HTMLElement) {
+		  static get define() {
+			  return {
+				  color: {
+					  default: "blue"
+				  }
+			  };
+		  }
 
-  let faves = new Faves();
-  faves.connectedCallback();
-});
+		constructor() {
+			super();
+			assert.equal(this.color, undefined, "color does not exist after constructor");
+			this.setup();
+			assert.equal(this.color, "blue", "color exists after setup is called manually");
+			this.color = "red";
+			assert.equal(this.color, "red", "color is changed to red");
+		}
+
+		connectedCallback() {
+			super.connectedCallback();
+			assert.equal(this.color, "red", "color is not redefined");
+		}
+	  }
+	  customElements.define("can-manually-defined-el", Faves);
+
+	  let faves = new Faves();
+	  faves.connectedCallback();
+	});
+}
