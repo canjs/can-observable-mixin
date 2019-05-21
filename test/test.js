9b634d2568f376ccee4400cb71d4d005e70c18c8
Matthew Phillips <matthew@matthewphillips.info>
2019-05-21 16:34:28 -0500

    Conditionally run the HTMLElement tests

--- test/test.js
+++ test/test.js
@@ -6,5 +6,10 @@ require("./define-mixin-default-test");
 require("./define-mixin-new-test");
 
 // Proxy stuff
+const supportsCustomElements = "customElements" in window;
+
 require("./define-mixin-proxy-object-test");
-require("./define-mixin-proxy-htmlelement-test");
+
+if(supportsCustomElements) {
+	require("./define-mixin-proxy-htmlelement-test");
+}
