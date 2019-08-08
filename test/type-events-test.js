6a401690929d7c2de26ec36581bb3c422607b5cb
Matthew Phillips <matthew@matthewphillips.info>
2019-08-08 08:28:11 -0400

    Add value, oldValue, action, key to event object
    This adds new properties to the events object, in anticipation of a
    future breaking change where the events object will be the only
    argument.

--- test/type-events-test.js
+++ test/type-events-test.js
@@ -8,3 +8,9 @@ QUnit.module("can-observable-mixin Type events");
 require("can-reflect-tests/observables/map-like/type/type")("Defined", function(){
 	return class Type extends mixinObject() {};
 });
+
+require("can-reflect-tests/observables/map-like/instance/on-event-get-set-delete-key")("ObservableObject", function(){
+	class Type extends mixinObject() {}
+
+	return new Type();
+});
