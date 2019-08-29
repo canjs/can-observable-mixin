28ac5359cbcb3155548e04866df4159d872fb78d
Nils Lundquist <nils@bitovi.com>
2019-08-29 13:40:06 -0600

    as per Justin's suggestion adding `can-event-queue/map/map` mixin to Type

--- src/mixin-typeevents.js
+++ src/mixin-typeevents.js
@@ -1,8 +1,10 @@
 const addTypeEvents  = require("can-event-queue/type/type");
+const addMapEvents  = require("can-event-queue/map/map");
 
 function mixinTypeEvents(Type) {
 	let Child = class extends Type {};
 	addTypeEvents(Child);
+	addMapEvents(Child);
 	return Child;
 }
 
