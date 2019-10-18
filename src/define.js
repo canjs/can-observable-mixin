ccdbf7800f858689370b63c4ce9bae32fc1a99ce
Cherif BOUCHELAGHEM <cherif.bouchelaghem@gmail.com>
2019-10-18 21:26:20 +0100

    Quote value in the type error message

--- src/define.js
+++ src/define.js
@@ -695,7 +695,7 @@ make = {
 								return set.call(this, canReflect.convert(newValue, type));
 							} catch (error) {
 								var typeName = canReflect.getName(type[baseTypeSymbol]);
-								var message  = newValue +  ' is not of type ' + typeName + '. Property ' + prop + ' is using "type: ' + typeName + '". ';
+								var message  = '"' + newValue + '"' +  ' is not of type ' + typeName + '. Property ' + prop + ' is using "type: ' + typeName + '". ';
 								message += 'Use "' + prop + ': type.convert(' + typeName + ')" to automatically convert values to ' + typeName + 's when setting the "' + prop + '" property.';
 								throw new Error(message);
 							}
